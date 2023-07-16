06907fd959c15d7257a6780fa4472e3dba660ae6
johypark97 <johypark97@gmail.com>
2023-07-16 18:11:42 +0900

    Add git completion script installer

---
+++ installer/git-completion.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# ======================
+# -------- main --------
+# ======================
+
+function main()
+{
+    if ! command -v curl > /dev/null; then
+        echo "curl is not installed"
+        exit 1
+    fi
+
+    local FILE=~/.git-completion.bash
+    local URL=https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+    curl -o $FILE $URL
+}
+
+main "$@"
